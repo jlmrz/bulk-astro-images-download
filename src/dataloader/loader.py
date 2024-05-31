9a8315591e7929143433ee2cc4fbf4f921c123f1
Julie Moroz <julie.prokasheva@gmail.com>
2024-05-31 08:41:36 +0300

    Switches to Path type for paths. Minor changes in making directories

--- src/dataloader/loader.py
+++ src/dataloader/loader.py
@@ -5,9 +5,10 @@ import numpy as np
 from numpy import ndarray 
 from tqdm import tqdm
 from typing import Tuple, Any
+from pathlib import Path
 
 from astropy import units as u
-from astropy.coordinates import Longitude, Latitude, Angle
+from astropy.coordinates import Longitude, Latitude
 from astropy.table import Table
 from astroquery.hips2fits import hips2fits
 
@@ -39,7 +40,7 @@ def download_image(ra, dec, config: Dictionary[str, Any]) -> ndarray:
     return img
 
 
-def process_object(objinfo: Tuple, config: Dictionary[str, Any], savepath: str) -> int:
+def process_object(objinfo: Tuple, config: Dictionary[str, Any], savepath: Path) -> int:
     """
     Arguments:
         objinfo: Tuple[objid, ra, dec, objtype, z, zerr]
@@ -48,39 +49,30 @@ def process_object(objinfo: Tuple, config: Dictionary[str, Any], savepath: str) 
         Status: 1 is success else 0
     """
     objid, ra, dec, objtype, z, zerr = objinfo
-    save_fname = '_'.join([savepath, objtype, str(objid).strip('[]').replace(' ', '')])
+    save_fname = '_'.join([objtype, str(objid).strip('[]').replace(' ', '')])
 
-    if not os.path.isfile(save_fname + '.npz'):
+    if not os.path.isfile(savepath / f'{save_fname}.npz'):
         try:
             img = download_image(ra, dec, config)
             np.savez_compressed(
-                save_fname, img=img, redshift=z, zerr=zerr
+                savepath / save_fname, img=img, redshift=z, zerr=zerr
             )
             return 1
 
         except ConnectionError as e:
-            logging.info(f'Connection error when downloading image: {e}')
+            logging.error(f'Connection error when downloading image: {e}')
             return 0
     else:
         logging.info(f'File {save_fname} already exists.')
         return 0
     
 
-def load_images(config: Dictionary[str, Any], objtype: str, path) -> None:
+def load_images(config: Dictionary[str, Any], objtype: str, path: Path) -> None:
 
-    if objtype not in ('QSO', 'GALAXY', 'STAR'):
-       raise Exception(f'Unknown object type: {objtype}.')
-    
-    path_to_save = path + 'cropped-images/'
-    if not os.path.exists(path_to_save):
-        os.makedirs(path_to_save)
-    
-    logging.info('Loading data..')
-    data = Table.read(path + objtype + '.fits', format='fits')
+    data = Table.read(path.parent / f'{objtype}.fits', format='fits')
 
     success_counter = 0
     for row in tqdm(data.iterrows()):
-        success_counter += process_object(row, config, path_to_save)
+        success_counter += process_object(row, config, path)
 
-    logging.info(f'Number of objects processed: {success_counter}')
-  
+    return success_counter
