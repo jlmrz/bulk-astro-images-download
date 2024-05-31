2d25f7ec747cb32299d545823c015b10b6796e32
Julie Moroz <julie.prokasheva@gmail.com>
2024-05-31 10:22:22 +0300

    Fix reloading existing file

--- jobs.sh
+++ jobs.sh
@@ -1,3 +1,3 @@
 mkdir ./data
-rsync -lv rsync://dtn.sdss.org/dr9/sdss/spectro/redux/specObj-dr9.fits ./data 
+rsync --info=progress2 -t -lv rsync://dtn.sdss.org/dr9/sdss/spectro/redux/specObj-dr9.fits ./data 
 python3 main.py routine='test' objects='QSO'
