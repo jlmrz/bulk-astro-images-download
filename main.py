5dd1996220066bb50c16ea0f67355ea3a5e7b07b
Julie Moroz <julie.prokasheva@gmail.com>
2024-06-14 11:52:19 +0300

    Add saving images to hdf5 file plus minor updates

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from pathlib import Path
 from omegaconf import DictConfig, OmegaConf
 
 from src.dataloader.select_objects import select_and_save
-from src.dataloader.loader import load_images
+from src.dataloader.loader import load_images_csv, load_images_fits, npz_to_hdf5
 from src.utils import Dictionary
 from astropy.coordinates import Angle
 from astropy import units as u
@@ -14,11 +14,12 @@ from astropy import units as u
 
 @hydra.main(version_base=None, config_path="config", config_name="main")
 def main(cfg: DictConfig):
-    routine, obj = cfg.routine, cfg.objects
+    routine, obj, source = cfg.routine, cfg.objects, cfg.data_source
 
-    assert obj in ('QSO', 'STAR', 'GALAXY')
-    assert routine in ('test', 'run')
+    assert obj in ('QSO', 'STAR', 'GALAXY') or routine in ('test', 'run')
+    assert source in ('csv', 'fits')
 
+    load_images = load_images_csv if source == 'csv' else load_images_fits
     num_objects = 10 if routine == 'test' else cfg.number_of_objects
 
     images_config = Dictionary(OmegaConf.to_container(cfg.images_config))
@@ -30,19 +31,28 @@ def main(cfg: DictConfig):
     if not os.path.exists(results_obj_dir):
         results_obj_dir.mkdir(parents=True, exist_ok=True)
 
-    if not os.path.exists(cfg.data_file):
+    if not os.path.exists(cfg.data_file) or cfg.overwrite:
         logging.info(f"Selecting and saving objects at {data_dir}")
         
         select_and_save(
-            objtype=obj, savepath=data_dir, nobj=num_objects
+            objtype=obj, savepath=data_dir, nobj=num_objects, source=source
         )
 
     logging.info(f"Loading images" )
 
-    success_counter = load_images(
-        config=images_config, objtype=obj, path=results_obj_dir
+    if routine != 'run':
+        success_counter = load_images(
+            config=images_config, objtype=obj, path=results_obj_dir
+        )
+
+        logging.info(f'Number of objects processed: {success_counter}')
+
+    logging.info(f'Creating hdf5 file..')
+
+    npz_to_hdf5(
+        img_dir=results_obj_dir
     )
-    logging.info(f'Number of objects processed: {success_counter}')
+
 
 
 if __name__ == '__main__':    
